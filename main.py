e57804d9323c629a77cea858ef5c7eecc7ee338c
mayurdhingra07 <mayurdhingra1997@gmail.com>
2023-07-01 20:12:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
 import streamlit as st
 import openai
-os.environ["OPENAI_API_KEY"] = "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"
+openai.api_key = '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'
 from chemcrow.agents import ChemCrow
 chem_model = ChemCrow(model="gpt-3.5-turbo-0613", temp=0.1, verbose=True)
 chem_model.run("What is the molecular weight of tylenol?")
